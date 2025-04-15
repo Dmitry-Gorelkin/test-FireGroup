1c28eff735fe12bc1f78ed55543e20b45a9ff530
Дмитрий Горелкин <gorelkind@gmail.com>
2025-04-15 12:35:25 +0300

    add_base-style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App.jsx';
+import { GlobalStyle } from './style/GlobalStyle.js';
+import { theme } from './style/theme.js';
+import { ThemeProvider } from 'styled-components';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
