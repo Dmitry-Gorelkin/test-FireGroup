e222c5ff443730ee57a7855dc3cbce4e0175d7e0
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2025-04-15 22:29:05 +0300

    fix

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 const testPost = [
   {
-    id: Date.now() + 500000,
+    id: 1744745773282,
     title: 'Charged Up',
     text: 'Never run low again — fast charging is changing the game. Stay powered, stay ahead.',
   },
   {
-    id: Date.now(),
+    id: 1744745473282,
     title: 'Smart Tech, Smarter Life',
     text: 'Upgrade your day with gadgets that work with you, not for you. Welcome to the future.',
   },
   {
-    id: Date.now() + 200000,
+    id: 1744745273282,
     title: 'Tiny Tech, Big Impact',
     text: 'Small devices, huge power. Pocket tech is reshaping how we live and work every day.',
   },
