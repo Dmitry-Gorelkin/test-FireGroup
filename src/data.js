8d5bbf0636ffb3e15681363f9e93abd1c5789149
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2025-04-15 22:23:07 +0300

    test

--- src/data.js
+++ src/data.js
@@ -1,21 +1,19 @@
-// const testPost = [
-//   {
-//     id: Date.now() + 500000,
-//     title: 'Charged Up',
-//     text: 'Never run low again — fast charging is changing the game. Stay powered, stay ahead.',
-//   },
-//   {
-//     id: Date.now(),
-//     title: 'Smart Tech, Smarter Life',
-//     text: 'Upgrade your day with gadgets that work with you, not for you. Welcome to the future.',
-//   },
-//   {
-//     id: Date.now() + 200000,
-//     title: 'Tiny Tech, Big Impact',
-//     text: 'Small devices, huge power. Pocket tech is reshaping how we live and work every day.',
-//   },
-// ];
-
-const testPost = [];
+const testPost = [
+  {
+    id: Date.now() + 500000,
+    title: 'Charged Up',
+    text: 'Never run low again — fast charging is changing the game. Stay powered, stay ahead.',
+  },
+  {
+    id: Date.now(),
+    title: 'Smart Tech, Smarter Life',
+    text: 'Upgrade your day with gadgets that work with you, not for you. Welcome to the future.',
+  },
+  {
+    id: Date.now() + 200000,
+    title: 'Tiny Tech, Big Impact',
+    text: 'Small devices, huge power. Pocket tech is reshaping how we live and work every day.',
+  },
+];
 
 export default testPost;
